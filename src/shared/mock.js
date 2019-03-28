b712e4a13e317c74566056f412ea789e0342ebfa
Sergei Karaulov <sixty.xi@gmail.com>
2019-03-29 01:11:05 +0400

    Task-3-Components Add shared resourses

---
+++ src/shared/mock.js
@@ -0,0 +1,79 @@
+export const MOVIES = [
+  {
+    id: 1,
+    title: 'Four rooms',
+    tagline: 'Oscar-winning Movies',
+    vote_average: 3,
+    release_date: '1995',
+    poster_path: 'https://images-na.ssl-images-amazon.com/images/I/51qcn-rYWqL._SY450_.jpg',
+    overview: `Don't miss the fun in this hilariously sexy comedy that has Antonio Banderas (Once Upon a Time in Mexico), Madonna (Swept Away), and a sizzling all-star cast checking in for laughs! It's Ted the Bellhop's (Tim Roth, Pulp Fiction) first night on the job...and the hotel's very unusual guests are about to place him in some outrageous predicaments.`,
+    runtime: 97,
+    genres: [ 'comedies' ]
+  },
+  {
+    id: 2,
+    title: 'Kill Bill: Vol. 1',
+    tagline: 'Oscar-winning Movies',
+    vote_average: 4.5,
+    release_date: '2003',
+    poster_path: 'https://images-na.ssl-images-amazon.com/images/I/71XwrZLWOBL._SY450_.jpg',
+    overview: `From groundbreaking writer and director Quentin Tarantino KILL BILL VOLUME 1 stars Uma Thurman, Lucy Liu, and Vivica A. Fox in an epic vendetta! Four years after taking a bullet in the head at her own wedding, The Bride emerges from a coma and decides it's time for payback! Having been gunned down by her former boss and his deadly squad of international assassins, it's a kill-or- be- killed fight.`,
+    runtime: 150,
+    genres: [ 'action', 'adventure' ]
+  },
+  {
+    id: 3,
+    title: 'Four rooms',
+    tagline: 'Oscar-winning Movies',
+    vote_average: 3,
+    release_date: '1995',
+    poster_path: 'https://images-na.ssl-images-amazon.com/images/I/51qcn-rYWqL._SY450_.jpg',
+    overview: `Don't miss the fun in this hilariously sexy comedy that has Antonio Banderas (Once Upon a Time in Mexico), Madonna (Swept Away), and a sizzling all-star cast checking in for laughs! It's Ted the Bellhop's (Tim Roth, Pulp Fiction) first night on the job...and the hotel's very unusual guests are about to place him in some outrageous predicaments.`,
+    runtime: 97,
+    genres: [ 'comedies' ]
+  },
+  {
+    id: 4,
+    title: 'Kill Bill: Vol. 1',
+    tagline: 'Oscar-winning Movies',
+    vote_average: 4.5,
+    release_date: '2003',
+    poster_path: 'https://images-na.ssl-images-amazon.com/images/I/71XwrZLWOBL._SY450_.jpg',
+    overview: `From groundbreaking writer and director Quentin Tarantino KILL BILL VOLUME 1 stars Uma Thurman, Lucy Liu, and Vivica A. Fox in an epic vendetta! Four years after taking a bullet in the head at her own wedding, The Bride emerges from a coma and decides it's time for payback! Having been gunned down by her former boss and his deadly squad of international assassins, it's a kill-or- be- killed fight.`,
+    runtime: 150,
+    genres: [ 'action', 'adventure' ]
+  },
+  {
+    id: 5,
+    title: 'Four rooms',
+    tagline: 'Oscar-winning Movies',
+    vote_average: 3,
+    release_date: '1995',
+    poster_path: 'https://images-na.ssl-images-amazon.com/images/I/51qcn-rYWqL._SY450_.jpg',
+    overview: `Don't miss the fun in this hilariously sexy comedy that has Antonio Banderas (Once Upon a Time in Mexico), Madonna (Swept Away), and a sizzling all-star cast checking in for laughs! It's Ted the Bellhop's (Tim Roth, Pulp Fiction) first night on the job...and the hotel's very unusual guests are about to place him in some outrageous predicaments.`,
+    runtime: 97,
+    genres: [ 'comedies' ]
+  },
+  {
+    id: 6,
+    title: 'Kill Bill: Vol. 1',
+    tagline: 'Oscar-winning Movies',
+    vote_average: 4.5,
+    release_date: '2003',
+    poster_path: 'https://images-na.ssl-images-amazon.com/images/I/71XwrZLWOBL._SY450_.jpg',
+    overview: `From groundbreaking writer and director Quentin Tarantino KILL BILL VOLUME 1 stars Uma Thurman, Lucy Liu, and Vivica A. Fox in an epic vendetta! Four years after taking a bullet in the head at her own wedding, The Bride emerges from a coma and decides it's time for payback! Having been gunned down by her former boss and his deadly squad of international assassins, it's a kill-or- be- killed fight.`,
+    runtime: 150,
+    genres: [ 'action', 'adventure' ]
+  },
+  {
+    id: 7,
+    title: 'Four rooms',
+    tagline: 'Oscar-winning Movies',
+    vote_average: 3,
+    release_date: '1995',
+    poster_path: 'https://images-na.ssl-images-amazon.com/images/I/51qcn-rYWqL._SY450_.jpg',
+    overview: `Don't miss the fun in this hilariously sexy comedy that has Antonio Banderas (Once Upon a Time in Mexico), Madonna (Swept Away), and a sizzling all-star cast checking in for laughs! It's Ted the Bellhop's (Tim Roth, Pulp Fiction) first night on the job...and the hotel's very unusual guests are about to place him in some outrageous predicaments.`,
+    runtime: 97,
+    genres: [ 'comedies' ]
+  }
+];
