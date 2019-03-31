9253133fac6913169319b31023a43ea9045552d5
Sergei Karaulov <sixty.xi@gmail.com>
2019-03-31 17:17:59 +0400

    Task-4-Testing Increase test coverage to 80 percent

---
+++ src/components/Header/Header.test.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import { Header } from './Header';
+
+describe('Header', () => {
+  test('renders correctly', () => {
+    const wrapper = shallow(<Header />);
+
+    expect(wrapper).toMatchSnapshot();
+  });
+});
