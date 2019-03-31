c9258c2e96654ba61bc1e9b661df5514550cad8a
Sergei Karaulov <sixty.xi@gmail.com>
2019-03-31 14:41:06 +0400

    Task-4-Testing Correct import path

--- src/movie/MoviePage/MoviePage.jsx
+++ src/movie/MoviePage/MoviePage.jsx
@@ -7,7 +7,7 @@ import { Logo } from '../../components/Logo/Logo';
 import { SearchBtn } from '../../components/SearchBtn/SearchBtn';
 import { ContentWrapper } from '../../components/ContentWrapper/ContentWrapper';
 import { SummaryInfo } from '../../components/SummaryInfo/SummaryInfo';
-import { MOVIES } from '../../shared/mock';
+import { MOVIES } from '../../shared/moviesMock';
 import { MovieCard } from '../components/MovieCard/MovieCard';
 
 const RESULT_INFO = 'Films by Drama genre';
