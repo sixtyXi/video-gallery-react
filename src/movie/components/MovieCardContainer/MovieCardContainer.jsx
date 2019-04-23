14bd9aa7cc7c72212d517e271af3128a5d9b5674
sixtyXi <sixty.xi@gmail.com>
2019-04-23 13:57:07 +0400

    Integrate MovieCardContainer with react router

--- src/movie/components/MovieCardContainer/MovieCardContainer.jsx
+++ src/movie/components/MovieCardContainer/MovieCardContainer.jsx
@@ -1,26 +1,44 @@
-import React, { PureComponent } from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { withRouter, Redirect } from 'react-router-dom';
 
 import { MovieCard } from '../MovieCard/MovieCard';
 import { fetchMovie } from '../../../actions/actions';
 
-export class MovieCardContainer extends PureComponent {
+export class MovieCardContainer extends Component {
   componentDidMount() {
-    const { getMovie } = this.props;
+    const {
+      getMovie,
+      match: {
+        params: { id }
+      }
+    } = this.props;
 
-    getMovie();
+    getMovie(id);
+  }
+
+  shouldComponentUpdate(nextProps) {
+    const { location, movie, getMovie } = this.props;
+    const id = movie ? movie.id : '';
+
+    if (location !== nextProps.location) {
+      getMovie(nextProps.match.params.id);
+    }
+
+    return id !== nextProps.movie.id;
   }
 
   render() {
     const { movie } = this.props;
 
-    return movie ? <MovieCard movie={movie} /> : null;
+    return Object.keys(movie).length === 0 ? <Redirect to="/404" /> : <MovieCard movie={movie} />;
   }
 }
 
 MovieCardContainer.propTypes = {
   movie: PropTypes.shape({
+    id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
     poster_path: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     vote_average: PropTypes.number.isRequired,
@@ -30,20 +48,33 @@ MovieCardContainer.propTypes = {
     overview: PropTypes.string.isRequired
   }),
   getMovie: PropTypes.func.isRequired,
-  id: PropTypes.number
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string
+    })
+  }).isRequired,
+  location: PropTypes.shape({
+    pathname: PropTypes.string
+  }).isRequired
 };
 
 MovieCardContainer.defaultProps = {
-  movie: null,
-  id: 680
+  movie: null
 };
 
-const mapStateToProps = state => ({ movie: state.moviePage.movie });
-const mapDispatchToProps = (dispatch, ownProps) => ({
-  getMovie: () => dispatch(fetchMovie(ownProps.id))
+const mapStateToProps = state => ({
+  movie: state.moviePage.movie
 });
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(MovieCardContainer);
+const mapDispatchToProps = dispatch => ({
+  getMovie: id => {
+    dispatch(fetchMovie(id));
+  }
+});
+
+export default withRouter(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(MovieCardContainer)
+);
