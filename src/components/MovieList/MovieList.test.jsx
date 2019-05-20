30b700b978be5d38e209052e2b013cf3df7c1132
Sergei Karaulov <sergei_karaulov@epam.com>
2019-05-20 15:41:46 +0400

    Fix tests

--- src/components/MovieList/MovieList.test.jsx
+++ src/components/MovieList/MovieList.test.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { fromJS } from "immutable";
 
 import { MovieList } from './MovieList';
 import { MovieThumb } from '../MovieThumb/MovieThumb';
@@ -9,19 +10,19 @@ describe('MovieList', () => {
   const movies = MOVIES.slice(0, 2);
 
   test('renders *No results found*', () => {
-    const wrapper = shallow(<MovieList />);
+    const wrapper = shallow(<MovieList movies={fromJS([])}/>);
 
     expect(wrapper).toMatchSnapshot();
   });
 
   test('renders correctly', () => {
-    const wrapper = shallow(<MovieList movies={movies} />);
+    const wrapper = shallow(<MovieList movies={fromJS(movies)} />);
 
     expect(wrapper).toMatchSnapshot();
   });
 
   test('renders two components *MovieThumb*', () => {
-    const wrapper = shallow(<MovieList movies={movies} />);
+    const wrapper = shallow(<MovieList movies={fromJS(movies)} />);
 
     expect(wrapper.find(MovieThumb).length).toBe(2);
   });
