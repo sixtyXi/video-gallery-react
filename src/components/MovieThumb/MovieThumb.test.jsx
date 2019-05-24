8d52fca34f8c713c2058dc38981dbc3471227452
sixtyXi <sixty.xi@gmail.com>
2019-05-24 09:04:43 +0400

    Fix tests

--- src/components/MovieThumb/MovieThumb.test.jsx
+++ src/components/MovieThumb/MovieThumb.test.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { fromJS } from 'immutable';
 
 import { MOVIES } from '../../shared/moviesMock';
 import { MovieThumb } from './MovieThumb';
 
 describe('MovieThumb', () => {
-  const movie = MOVIES[1];
+  const movie = fromJS(MOVIES[1]);
 
   test('renders correctly', () => {
     const wrapper = shallow(<MovieThumb movie={movie} />);
